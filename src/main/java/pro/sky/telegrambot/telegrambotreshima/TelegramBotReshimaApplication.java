462b135ebe0ebf6a579832b56bd72ec71a8f8d04
AndreevSV <andreevsv@gmail.com>
2023-05-20 21:22:22 +0300

    Initial commit

---
+++ src/main/java/pro/sky/telegrambot/telegrambotreshima/TelegramBotReshimaApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.telegrambot.telegrambotreshima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotReshimaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotReshimaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/telegrambot/telegrambotreshima/TelegramBotReshimaApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.telegrambot.telegrambotreshima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotReshimaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
